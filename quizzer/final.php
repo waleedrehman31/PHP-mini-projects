5f0f4edb4093c6812a7f1eefcb115c68cf775254
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-09-17 14:19:13 +0500

    Completing the final.php

---
+++ quizzer/final.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>PHP QUIZERR</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1>PHP Quizer</h1>
+		</div>
+	</header>
+	<main>
+		<div class="container">
+			<h2>You're Done</h2>
+			<p>Congrats! You have completed the test</p>
+			<p>Final Score: 5</p>
+			<a href="questions.php?n=1" class="start-quiz">Start Quiz</a>
+			
+		</div>
+	</main>
+	<footer>
+		<div class="container">
+			Copyright &copy; 2021 PHP Quizer
+		</div>
+	</footer>
+</body>
+</html>
