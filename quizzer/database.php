8488b827e6ce2a28f975ce36c6e6602b9c7ea099
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-09-17 19:12:01 +0500

    Create database.php and connect to database

---
+++ quizzer/database.php
@@ -0,0 +1,18 @@
+<?php 
+
+//Create Connection Crendentials
+	$db_host = "localhost";
+	$db_username =  'root';
+	$db_password = '';
+	$db_name = "quizzer";
+
+//Create MYSQLI Object
+	$mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+//Test connection
+	if ($mysqli->connect_error) {
+		printf("Connection Failed %s\n", $mysqli->connect_error);
+		exit();
+	}
+
+ ?>
