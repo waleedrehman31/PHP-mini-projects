8ebbda60dcdbc45711023e9efacd911ce4e0c172
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-09-17 09:37:20 +0500

    Creating question.php file and copying index.php

---
+++ quizzer/questions.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>PHP QUIZERR</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1>PHP Quizer</h1>
+		</div>
+	</header>
+	<main>
+		<div class="container">
+			<h2>Test Your PHP Knowledge</h2>
+			<p>This is a multiple choice question to test your knowledge of PHP</p>
+			<ul>
+				<li><strong>Number of Questions: </strong>5</li>
+				<li><strong>Type: </strong>Multiple choice</li>
+				<li><strong>Estimate Time: </strong>4 Minutes</li>
+			</ul>
+			<a href="questions.php?n=1" class="start-quiz">Start Quiz</a>
+		</div>
+	</main>
+	<footer>
+		<div class="container">
+			Copyright &copy; 2021 PHP Quizer
+		</div>
+	</footer>
+</body>
+</html>
