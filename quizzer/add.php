acd82845e6c29dd161497ecfd093f7a8d1b6f4ad
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-09-22 22:41:15 +0500

    Adding Question Form and Insert Data into Database

--- quizzer/add.php
+++ quizzer/add.php
@@ -1,3 +1,58 @@
+<?php include 'database.php'; ?>
+
+<?php 
+	if (isset($_POST['submit'])) {
+		//GET POST VARIALBES
+		$question_number = $_POST['question_number'];
+		$question_text = $_POST['question_text'];
+		$choices = array();
+		$choices[1] = $_POST['choice1'];
+		$choices[2] = $_POST['choice2'];
+		$choices[3] = $_POST['choice3'];
+		$choices[4] = $_POST['choice4'];
+		$choices[5] = $_POST['choice5'];
+		$correct_choice = $_POST['correct_choice'];
+
+		//Question Query
+		$query = "INSERT INTO questions (question_number, text) VALUES ('$question_number', '$question_text')";
+		// RUN
+		$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+		//Validate The Insert
+		if ($insert_row) {
+			foreach ($choices as $choice => $value) {
+				if ($value !== "") {
+					if ($correct_choice == $choice) {
+						$is_correct = 1;
+					} else {
+						$is_correct = 0;
+					}
+					//choice query
+					$query = "INSERT INTO choices (question_number, is_correct, text) VALUES ('$question_number', '$is_correct', '$value')";
+					//Run Query
+					$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+					//Validate The Insert
+					if ($insert_row) {
+						continue;
+					} else {
+						die('Error : ('. $mysqli->errno .')'. $mysqlii->error);
+					}
+				}
+			}
+			$message = "Question Has Been Added";
+		}
+	}	
+
+	/*
+	* Get Total Questions
+	*/
+	$query = "SELECT * FROM questions";
+	// Get Results
+	$results = $mysqli->query($query) or die($mysqli->error.__LINE__);
+	$total = $results->num_rows;
+	$next = $total+1;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,10 +69,13 @@
 	<main>
 		<div class="container">
 			<h2>Add A Question</h2>
+			<?php if(isset($message)) {
+				echo '<p>' . $message . '</p>';
+			} ?>
 			<form method="POST" action="add.php">
 				<p>
 					<label>Qestion Number: </label>
-					<input type="number" name="question_number" />
+					<input type="number" value="<?php echo $next ?>" name="question_number" />
 				</p>
 				<p>
 					<label>Qestion Text: </label>
