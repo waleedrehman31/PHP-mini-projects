a5bfe2abd08e40d57534f2aa27f36fe4f6776c42
waleedrehman31 <waleedrehman31@gmail.com>
2023-08-21 23:24:49 +0500

    add new project and complete signup page with database connected

---
+++ login_registration/process_signup.php
@@ -0,0 +1,45 @@
+<?php
+
+if (empty($_POST['name']))
+  die('Name Field is Required');
+
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+  die('Please enter valid email');
+
+if (strlen($_POST["password"]) < 8)
+  die("Password length must be 8");
+
+if (!preg_match('/[a-z]/i', $_POST['password']))
+  die("Password must have at least one alphabet");
+
+if (!preg_match('/[0-9]/i', $_POST['password']))
+  die("Password must have at least one number");
+
+if ($_POST['password'] != $_POST['password_confirmation'])
+  die("Password does not match");
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO user (name, email, password_hash) 
+        VALUES (?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if (!$stmt->prepare($sql)) {
+  die('SQL Error:' . $mysqli->error);
+}
+
+$stmt->bind_param(
+  'sss',
+  $_POST['name'],
+  $_POST['email'],
+  $password_hash
+);
+
+if ($stmt->execute()) {
+  echo "Submit Successfully";
+} else {
+  die($mysqli->error . "" . $mysqli->errno);
+}
