a7d5592fc7841621027916df97a6876a1106ab3f
waleedrehman31 <waleedrehman31@gmail.com>
2023-10-09 14:50:43 +0500

    add new project notes

---
+++ php-notes/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'notes_app_db',
+        'charset' => 'utf8mb4'
+    ],
+
+    //
+];
---
+++ php-notes/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+require 'router.php';
+
+
+
+//
+//$id = $_GET['id'];
+//$query = "select * from posts where id = :id";
+//
+//$posts = $db->query($query, [':id' => $id])->fetch();
+//
+//dd($posts);
